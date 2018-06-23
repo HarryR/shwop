0de80610d3995fb59b4bd1093e8a2c92e222bd76
Harry <gpg-dev-cm@midnight-labs.org>
2018-06-24 00:49:50 +0100

    Fixed Python binary distribution

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from pip.req import parse_requirements
 from pip.download import PipSession
 
@@ -6,7 +6,7 @@ from pip.download import PipSession
 setup(
 	name='shwop',
 	version='0.1',
-	packages=['shwop'],
+	packages=find_packages(),
 	py_modules=['__main__'],
 	python_requires='>3.5.0',
     install_requires=[str(ir.req) for ir in parse_requirements('requirements.txt', session=PipSession())],
